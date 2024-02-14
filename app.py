302f424a60bdcedae5c4026e2f430b4462077bd7
JulienHJJ <135319658+JulienHJJ@users.noreply.github.com>
2024-02-14 11:11:35 +0100

    Modification de app.py qui ajoute maintenant la méthode index() (qui va appeler la page affichage_livres.html déjà créée)
    Elle ajoute aussi la méthode ajouter_livre() qui permet d'ajouter un livre quand on appuie sur le bouton. Donc dans le html il va repérer titre et auteur qui sont les variables de la request.form
    
    Pareil pour modifier_livre() qui permet de modifier un livre et auteur et déclenche une série d'évènements si on clique dessus.
    
    Pareil pour supprimer_livre(), dédié à la suppression d'un livre et son auteur.

--- app.py
+++ app.py
@@ -1,13 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 from crud import LivreRepository
 
 app = Flask(__name__)
 repo_livre = LivreRepository()
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('affichage_livres.html', livres=repo_livre.lire_tous())
+
+@app.route('/ajouter', methods=['POST'])
+def ajouter_livre():
+    titre = request.form['titre']
+    auteur = request.form['auteur']
+    repo_livre.creer(titre, auteur)  # Utilisation de repo_livre pour créer un nouveau livre
+    return render_template('affichage_livres.html', livres=repo_livre.lire_tous())
+
+@app.route('/modifier/<int:id>', methods=['POST'])
+def modifier_livre(id):
+    nouveau_titre = request.form['nouveau_titre']
+    nouveau_auteur = request.form['nouveau_auteur']
+    repo_livre.mettre_a_jour(id, nouveau_titre, nouveau_auteur)
+    return redirect('/')
+
+@app.route('/supprimer/<int:id>', methods=['POST'])
+def supprimer_livre(id):
+    repo_livre.supprimer(id)
+    return redirect('/')
 
 
 if __name__ == '__main__':
